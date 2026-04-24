0139760b4b0af735d90b30bd037f9c0599ceea44
Pallavi Pattanashetti <Pallavi.Pattanashetti@gmail.com>
2026-04-24 13:21:44 -0700

    done

--- employee-dashboard-students-main/app/page.tsx
+++ employee-dashboard-students-main/app/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
-const Home = () => redirect('/login');
+const Home = () => {
+    redirect('/login');
+    return (
+        <></>
+    );
+};
 
 export default Home;
